1556b72e00fe19ac9ce92e8d74f2f60d11ffde7a
Koteswar97 <mannamkoti@gmail.com>
2025-08-29 23:50:31 +0530

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,7 +5,7 @@ BUCKET_NAME="koteshbucket-767397794296123456789"
 TIMESTAMP=$(date +%F-%H-%M)
 BACKUP_FILE="/tmp/jenkins-backup-$TIMESTAMP.tar.gz"
 
-tar -czvf $BACKUP_FILE /var/lib/jenkins
+tar -cvf jenkins_backup.tar --exclude='/var/lib/jenkins/workspace/backpipeline@tmp/durable-c6e9ac2c/jenkins-log.txt' /var/lib/jenkins/
 aws s3 cp $BACKUP_FILE s3://$BUCKET_NAME/
 rm -f $BACKUP_FILE
 
